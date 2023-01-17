3dd9892144338524876ea317563ec89899c5e57e
chayzat <chayzat.oorzhak.2001@mail.ru>
2023-01-17 22:42:06 +0300

    feat: load data

--- script.js
+++ script.js
@@ -11,4 +11,20 @@ async function getRates() {
 }
 
 getRates()
+//load data
+window.addEventListener('load', () => {
+    const listRate = document.getElementById('list')
+    const selectRate = document.getElementById('select')
+    let list = ratesArray["0"]
+    let result = list.map((item) => {
+        let itemRate = document.createElement('li')
+        itemRate.className = 'swiper-slide rate__item'
+        itemRate.textContent = `${item.CharCode} - ${(item.Value).toFixed(2)}`
+        //
+        let option = document.createElement('option')
+        option.textContent = `${item.CharCode}`
+        selectRate.appendChild(option)
 
+        return listRate.appendChild(itemRate)
+    })
+})
