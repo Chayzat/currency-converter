1452bbdb93857eca778cdba536e2b4ef3a60a441
chayzat <chayzat.oorzhak.2001@mail.ru>
2023-01-17 22:50:19 +0300

    fix: response link

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const result = document.getElementById('result')
 const select = document.getElementById('select')
 
 async function getRates() {
-    const data = await (await fetch('https://www.cbr-xml-daily.ru/daily_json.js')).json()
+    const response = await fetch('https://www.cbr-xml-daily.ru/daily_json.js')
+    const data = await response.json()
     const result = await data
 
     let valute = result.Valute
