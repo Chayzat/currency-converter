8d7f34dfee75adf0347ae52b1ada99fd542973af
chayzat <chayzat.oorzhak.2001@mail.ru>
2023-01-17 22:43:18 +0300

    feat: currencies swiper

--- script.js
+++ script.js
@@ -28,3 +28,15 @@ window.addEventListener('load', () => {
         return listRate.appendChild(itemRate)
     })
 })
+
+//swiper currencies
+let swiper = new Swiper('.mySwiper', {
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination"
+    },
+    slidesPerView: 3,
+    mousewhell: true,
+    keyboard: true
+})
